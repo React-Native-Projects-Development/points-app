0f8ec7f4947bedfd645637a1aaf4dbdd6df7765a
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-30 06:36:43 -0600

    add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import {AppNavigator} from './src/navigation/root_navigator/AppNavigator';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <>
+      <StatusBar
+        barStyle={'dark-content'}
+        backgroundColor="rgba(0,0,0,0)"
+        translucent
+      />
+      <AppNavigator />
+    </>
+  );
 };
 
 export default App;
