4d7fff11ffc7f90705e74f0f86763fbf3ffdebb5
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-30 05:36:04 -0600

    add base product details screen and default styles

---
+++ src/screens/product_detail/ProductDetailScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import styles from './styles';
+
+export const ProductDetails = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ProductDetails</Text>
+    </View>
+  );
+};
