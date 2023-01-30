615ed27ed6e4bf03f17dd06042d9dcacd32461af
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-30 06:39:01 -0600

    add ProductDetailtScreen content and update styles

--- src/screens/product_detail/ProductDetailScreen.tsx
+++ src/screens/product_detail/ProductDetailScreen.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+
+import {useNavigation, useRoute} from '@react-navigation/native';
 
 import styles from './styles';
+import {FadeInImage} from '../../components/image/FadeInImage';
+import {
+  ProductScreenNavigationProp,
+  RootRouteProps,
+} from '../../navigation/stack_navigator/types';
+import {formatDate} from '../../utils/utils';
 
-export const ProductDetails = () => {
+const ProductDetailScreen = () => {
+  const navigation = useNavigation<ProductScreenNavigationProp>();
+  const route = useRoute<RootRouteProps<'ProductDetailsScreen'>>();
+  const {createdAt, image, points, name} = route.params;
+
   return (
-    <View style={styles.container}>
-      <Text>ProductDetails</Text>
-    </View>
+    <ScrollView
+      style={styles.scrollview}
+      alwaysBounceVertical={false}
+      showsVerticalScrollIndicator={false}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>{name}</Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <View style={styles.imgContainer}>
+          <FadeInImage uri={image} style={styles.img} />
+        </View>
+        <Text style={styles.greyText}>Detalles del producto:</Text>
+        <Text style={styles.descriptionText}>
+          Comprado el {formatDate(createdAt)}
+        </Text>
+        <Text style={styles.pointsDescriptionText}>
+          Con esta compra acumulaste:
+        </Text>
+        <Text style={styles.pointsText}>{points.toLocaleString()} puntos</Text>
+
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={() => navigation.goBack()}
+          style={styles.btn}>
+          <Text style={styles.btnText}>Aceptar</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
+
+export default ProductDetailScreen;
--- src/screens/product_detail/styles.ts
+++ src/screens/product_detail/styles.ts
@@ -1,9 +1,91 @@
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
+
+import {colors} from '../../constants/colors';
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from '../../utils/metrics';
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
+  scrollview: {flex: 1},
+  header: {
+    height: verticalScale(150),
+    backgroundColor: '#CFD6FF',
+    justifyContent: 'flex-end',
+    paddingHorizontal: horizontalScale(20),
+  },
+  headerText: {
+    fontFamily: 'Avenir-Book',
+    fontSize: moderateScale(24),
+    fontWeight: '800',
+    color: '#000',
+    marginBottom: verticalScale(24),
+  },
+  contentContainer: {marginHorizontal: horizontalScale(20)},
+  imgContainer: {
+    marginTop: verticalScale(19),
+    backgroundColor: '#fff',
+    height: verticalScale(350),
+    borderRadius: moderateScale(10),
+    ...Platform.select({
+      android: {
+        elevation: 8,
+      },
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: verticalScale(4),
+        },
+        shadowRadius: moderateScale(4),
+        shadowOpacity: 0.5,
+      },
+    }),
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+    borderRadius: moderateScale(10),
+  },
+  greyText: {
+    marginTop: verticalScale(32),
+    fontSize: moderateScale(14),
+    fontWeight: '800',
+    color: '#9B9898',
+  },
+  descriptionText: {
+    marginTop: verticalScale(19),
+    fontSize: moderateScale(16),
+    fontWeight: '800',
+    color: '#000',
+  },
+  pointsDescriptionText: {
+    marginTop: verticalScale(20),
+    fontSize: moderateScale(14),
+    fontWeight: '800',
+    color: '#9B9898',
+  },
+  pointsText: {
+    marginTop: verticalScale(32),
+    fontSize: moderateScale(24),
+    fontWeight: '800',
+    color: '#000',
+  },
+  btn: {
     justifyContent: 'center',
-    alignItems: 'center',
+    alignContent: 'center',
+    backgroundColor: colors.primary,
+    marginTop: verticalScale(47),
+    width: '100%',
+    height: 50,
+    borderRadius: moderateScale(10),
+    marginBottom: verticalScale(40),
+  },
+  btnText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: '800',
+    fontSize: moderateScale(16),
   },
 });
