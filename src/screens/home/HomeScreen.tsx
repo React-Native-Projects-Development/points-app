b080257158b18fc06f1f86e141cdb68f9b6fa64b
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-30 06:38:44 -0600

    add HomeScreen content and update styles

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,11 +1,99 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, Text, FlatList, ActivityIndicator} from 'react-native';
+
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+import {colors} from '../../constants/colors';
+import {CustomButton, ListItem} from '../../components';
+import {EmptyContainer} from '../../containers/empty/EmptyContainer';
+import {horizontalScale, verticalScale} from '../../utils/metrics';
+import {Product} from '../../interfaces/product';
+import {RootStackParamList} from '../../navigation/stack_navigator/types';
+import {useProducts} from '../../hooks';
 import styles from './styles';
 
-export const HomeScreen = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'HomeScreen'>;
+
+const HomeScreen: React.FC<Props> = ({navigation}) => {
+  const {
+    filteredData,
+    isLoading,
+    selectedOption,
+    setSelectedOption,
+    totalPoints,
+    isCalculatingPoints,
+  } = useProducts();
+
+  const onPress = (item: Product) =>
+    navigation.navigate('ProductDetailsScreen', {
+      createdAt: item.createdAt,
+      image: item.image,
+      points: item.points,
+      name: item.product,
+    });
+
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.textTitle}>Bienvenido de vuelta!</Text>
+        <Text style={styles.textSubtitle}>Ruben Rodriguez</Text>
+      </View>
+      <Text style={styles.greyTitle}>Tus Puntos</Text>
+      <View style={styles.cardContainer} testID="points-card">
+        <Text style={styles.cardText}>Diciembre</Text>
+        {isCalculatingPoints ? (
+          <ActivityIndicator
+            testID="points-loading-indicator"
+            color={colors.white}
+            style={styles.cardPoints}
+          />
+        ) : (
+          <Text testID="total-points" style={styles.cardPoints}>
+            {totalPoints.toLocaleString()} pts
+          </Text>
+        )}
+      </View>
+      <Text style={styles.greyTitle}>Tus Movimientos</Text>
+      <View style={styles.flatlistContainer}>
+        <FlatList
+          testID="products-list"
+          keyExtractor={item => item.id.toString()}
+          data={filteredData}
+          ListEmptyComponent={<EmptyContainer isLoading={isLoading} />}
+          contentContainerStyle={{
+            paddingVertical: verticalScale(20),
+            paddingLeft: horizontalScale(10),
+          }}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) => (
+            <ListItem item={item} onPress={() => onPress(item)} />
+          )}
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        {selectedOption !== 'all' ? (
+          <CustomButton
+            style={styles.btnAll}
+            onPress={() => setSelectedOption('all')}>
+            <Text style={styles.btnText}>Todos</Text>
+          </CustomButton>
+        ) : (
+          <>
+            <CustomButton
+              style={{...styles.singleBtn, marginRight: horizontalScale(13)}}
+              onPress={() => setSelectedOption('earned')}>
+              <Text style={styles.btnText}>Ganados</Text>
+            </CustomButton>
+            <CustomButton
+              style={styles.singleBtn}
+              onPress={() => setSelectedOption('exchanged')}>
+              <Text style={styles.btnText}>Canjeados</Text>
+            </CustomButton>
+          </>
+        )}
+      </View>
     </View>
   );
 };
+
+export default HomeScreen;
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,9 +1,115 @@
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
+
+import {colors} from '../../constants/colors';
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from '../../utils/metrics';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
+  },
+  center: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  welcomeContainer: {
+    marginTop: verticalScale(60),
+    marginLeft: horizontalScale(20),
+    marginBottom: verticalScale(20),
+  },
+  textTitle: {
+    fontFamily: 'Avenir-Book',
+    color: '#000',
+    fontWeight: '800',
+    fontSize: moderateScale(20),
+    lineHeight: verticalScale(27.32),
+  },
+  textSubtitle: {
+    fontFamily: 'Avenir-Book',
+    color: '#000',
+    fontWeight: '400',
+    fontSize: moderateScale(16),
+    lineHeight: verticalScale(21.86),
+  },
+  greyTitle: {
+    marginLeft: horizontalScale(20),
+    fontWeight: '800',
+    fontFamily: 'Avenir-Book',
+    fontSize: moderateScale(14),
+    color: '#9B9898',
+    textTransform: 'uppercase',
+  },
+  cardContainer: {
+    backgroundColor: colors.primary,
+    alignSelf: 'center',
+    width: horizontalScale(286),
+    height: verticalScale(143),
+    marginVertical: verticalScale(20),
+    borderRadius: moderateScale(20),
+    paddingVertical: verticalScale(21),
+    paddingHorizontal: horizontalScale(19),
+    ...Platform.select({
+      android: {
+        elevation: 15,
+      },
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: verticalScale(4),
+        },
+        shadowRadius: moderateScale(4),
+        shadowOpacity: 0.5,
+      },
+    }),
+  },
+  cardText: {
+    color: '#fff',
+    fontFamily: 'Avenir-Book',
+    fontWeight: '800',
+    fontSize: moderateScale(16),
+    lineHeight: verticalScale(21.86),
+  },
+  cardPoints: {
+    color: '#fff',
+    fontFamily: 'Avenir-Book',
+    fontWeight: '800',
+    fontSize: moderateScale(32),
+    lineHeight: verticalScale(43.71),
+    textAlign: 'center',
+    marginTop: verticalScale(7),
+  },
+  flatlistContainer: {
+    marginTop: verticalScale(20),
+    marginHorizontal: horizontalScale(20),
+    backgroundColor: '#fff',
+    height: verticalScale(350),
+    borderRadius: moderateScale(10),
+  },
+  buttonsContainer: {
+    marginTop: verticalScale(43),
+    marginHorizontal: horizontalScale(20),
+    marginBottom: verticalScale(40),
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  btnAll: {
+    height: verticalScale(50),
+    borderRadius: moderateScale(10),
+    width: '100%',
+  },
+  btnText: {
+    fontFamily: 'Avenir-Book',
+    fontWeight: '800',
+    fontSize: moderateScale(16),
+    color: '#fff',
+  },
+  singleBtn: {
+    height: verticalScale(50),
+    width: horizontalScale(170),
   },
 });
